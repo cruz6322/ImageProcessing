577b8d28e21719e51e2d872d4858a536015d911e
cruz6322 <41175783+cruz6322@users.noreply.github.com>
2026-02-02 16:45:05 +0900

    Add files via upload

---
+++ 99.py
@@ -0,0 +1,15 @@
+
+
+for i in range(1,10):
+    print("[{}]단 ".format(i))
+    
+    for j in range(1,10):
+        k = i * j
+        print("{} x {} = {}".format(i, j, k ))
+        
+        j += 1
+        
+    i += 1
+        
+    
+        
